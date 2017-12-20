daf165c165b1f0f95f41a071ff0ec9b099fad116
Tom Stephens <thomas.stephens@gmail.com>
2017-12-20 14:18:19 -0700

    Remove debugging code from save() & load() methods

--- src/core/FPObject.cpp
+++ src/core/FPObject.cpp
@@ -18,17 +18,17 @@ FPObject::~FPObject(){
 }
 
 const int FPObject::writeString(std::ostream &os, const std::string &s) const {
-	std::streampos pos = os.tellp();
+//	std::streampos pos = os.tellp();
 	write(os,s.size());
 	os << s.c_str();
-	os.flush();
+//	os.flush();
 	return 0;
 }
 
 int FPObject::readString(std::istream &is, std::string &s){
 	// get the size of the string
 	size_t size;
-	std::streampos pos = is.tellg();
+//	std::streampos pos = is.tellg();
 	read(is,size);
 	// now read in the string itself
 	if (size >0 ){ // but only if there is data to read
